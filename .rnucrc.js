54f977ebaccfa712d099a923534b1428193cbc60
Rik <ik@rikvanduijn.nl>
2020-07-20 17:45:40 +0200

    Split ios icon assets

--- .rnucrc.js
+++ .rnucrc.js
@@ -3,16 +3,26 @@ var ncp = require('ncp').ncp;
 module.exports = {
   on_env: async function (env) {
     // Copy Android res folder
-    const sourceFolder = `./android/app/src/${env.RES_ID}/`;
-    const destFolder = "./android/app/src/main/";
+    const resSourceFolder = `./android/app/src/${env.RES_ID}/`;
+    const resDestFolder = "./android/app/src/main/";
     ncp(
-      sourceFolder,
-      destFolder,
+      resSourceFolder,
+      resDestFolder,
       err => {
         if (err) {
           return console.error(err);
         }
         console.log('copied android resources!');
       })
+
+    // Copy iOS icon set
+    const xcassetsSourceFolder = `./ios/RedVSBlue2/Images.xcassets/${env.ICON_SET_NAME}.appiconset/`;
+    const xcassetsDestFolder = './ios/RedVSBlue2/Images.xcassets/AppIcon.appiconset/';
+    ncp(xcassetsSourceFolder, xcassetsDestFolder, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log('copied ios xcassets!');
+    });
   },
 };
